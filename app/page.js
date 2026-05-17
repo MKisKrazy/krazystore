7724dc1bc402ed94a6a83b686bb33e46331a5416
Mkiskrazy <mahihaasan123@gmail.com>
2026-05-17 16:39:22 +0530

    added force dynamic to stop prerender 1

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import ImageBanner from "@/components/ImageBanner";
 import Products from "@/components/Products";
 import { getProducts } from "../lib/prod.js"
 
-const dynamic = 'force-dynamic'
+export const dynamic = 'force-dynamic'
 
 // export async function getProducts() {
 //   const baseURL = process.env.NEXT_PUBLIC_BASE_URL
