0ad2f8ab11480b61710305a7c78bc5ee43e692e7
Mkiskrazy <mahihaasan123@gmail.com>
2026-05-17 16:26:25 +0530

    build issue n emailjs remove

--- components/EmailInput.jsx
+++ components/EmailInput.jsx
@@ -1,38 +1,38 @@
 'use client'
 
 import React, { useRef, useState } from 'react';
-import emailjs from '@emailjs/browser';
+// import emailjs from '@emailjs/browser';
 
 export default function EmailInput() {
     const [email, setEmail] = useState('')
-    const form = useRef();
+    // const form = useRef();
 
-    const sendEmail = (e) => {
-        e.preventDefault();
+    // const sendEmail = (e) => {
+    //     e.preventDefault();
 
-        emailjs.sendForm(
-            'NEXT_PUBLIC_EMAILJS_SERVICE_ID',
-            'NEXT_PUBLIC_EMAILJS_TEMPLATE_ID',
-            form.current,
-            'NEXT_PUBLIC_EMAILJS_PUBLIC_KEY'
-        )
-            .then((result) => {
-                console.log('Email sent successfully!', result.text);
-            }, (error) => {
-                console.log('Failed to send email...', error.text);
-            });
-    };
+    //     emailjs.sendForm(
+    //         'NEXT_PUBLIC_EMAILJS_SERVICE_ID',
+    //         'NEXT_PUBLIC_EMAILJS_TEMPLATE_ID',
+    //         form.current,
+    //         'NEXT_PUBLIC_EMAILJS_PUBLIC_KEY'
+    //     )
+    //         .then((result) => {
+    //             console.log('Email sent successfully!', result.text);
+    //         }, (error) => {
+    //             console.log('Failed to send email...', error.text);
+    //         });
+    // };
 
     return (
 
-        <form ref = { form } onSubmit = { sendEmail } >
+        // <form ref = { form } onSubmit = { sendEmail } >
             <div className="sign-up" >
                 <input value={email} placeholder="Email Address..." onChange={(e) => {
                     setEmail(e.target.value)
                 }} />
                 <button type="submit" className="button-card">Sign Up</button>
             </div >
-        </form >
+        // </form >
         
     )
 }
